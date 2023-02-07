e7ee8c20426586963423cc3d480fa90fd2091256
khludina <khlksen@gmail.com>
2023-02-07 11:56:05 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+def window():
+    """
+
+    function is drawing window
+    :return:
+    """
+
+    pass
+
+
+def window():
+    """
+
+    function is drawing window
+    :return:
+    """
+
+    pass
